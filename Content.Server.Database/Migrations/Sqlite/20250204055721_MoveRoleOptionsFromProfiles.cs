abbe13c7e3910d13a93e86c3e27d16148870b96d
Jen Pollock <jen@jenpollock.ca>
2025-02-04 21:47:28 -0700

    improve temp table handling, flesh out down migration

--- Content.Server.Database/Migrations/Sqlite/20250204055721_MoveRoleOptionsFromProfiles.cs
+++ Content.Server.Database/Migrations/Sqlite/20250204055721_MoveRoleOptionsFromProfiles.cs
@@ -37,7 +37,7 @@ UPDATE preference
             /* For each player, keep the highest priority from any profile for each job,
              * treating High priority as Medium */
             migrationBuilder.Sql(@"
-CREATE TEMP TABLE pref_job_temp
+CREATE TEMP TABLE preference_job_temp
 AS SELECT preference_id, job_name, min(2, max(priority)) priority
   FROM job
     JOIN profile ON job.profile_id = profile.profile_id
@@ -46,10 +46,10 @@ AS SELECT preference_id, job_name, min(2, max(priority)) priority
 
             /* Keep the High priority from each player's currently selected profile */
             migrationBuilder.Sql(@"
-UPDATE pref_job_temp
+UPDATE preference_job_temp
   SET priority = 3
   FROM job, profile, preference
-  WHERE pref_job_temp.preference_id = profile.preference_id
+  WHERE preference_job_temp.preference_id = profile.preference_id
     AND profile.profile_id = job.profile_id
     AND profile.slot = preference.selected_character_slot
     AND job.priority = 3;
@@ -82,8 +82,10 @@ UPDATE pref_job_temp
 
             migrationBuilder.Sql(@"
 INSERT INTO job(preference_id, job_name, priority)
-SELECT preference_id, job_name, priority FROM pref_job_temp;
+SELECT preference_id, job_name, priority FROM preference_job_temp;
 ");
+
+            migrationBuilder.Sql("DROP TABLE preference_job_temp;");
 
             migrationBuilder.DropForeignKey(
                 name: "FK_antag_profile_profile_id",
@@ -91,10 +93,10 @@ SELECT preference_id, job_name, priority FROM pref_job_temp;
 
             /* Combine each player's antag preferences across all their profiles */
             migrationBuilder.Sql(@"
-CREATE TEMP TABLE pref_antag_temp
+CREATE TEMP TABLE preference_antag_temp
 AS SELECT DISTINCT preference_id, antag_name
-  FROM job
-    JOIN profile ON job.profile_id = profile.profile_id;
+  FROM antag
+    JOIN profile ON antag.profile_id = profile.profile_id;
 ");
 
             migrationBuilder.Sql("DROP FROM antag;");
@@ -119,24 +121,49 @@ AS SELECT DISTINCT preference_id, antag_name
 
             migrationBuilder.Sql(@"
 INSERT INTO antag(preference_id, antag_name)
-SELECT preference_id, antag_name, priority FROM pref_antag_temp;
+SELECT preference_id, antag_name, priority FROM preference_antag_temp;
 ");
+
+            migrationBuilder.Sql("DROP TABLE preference_antag_temp;");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_antag_preference_preference_id",
-                table: "antag");
+            /* For lack of better ideas, copy each preference's role options to each
+             * of its profiles */
+
+            migrationBuilder.AddColumn<int>(
+                name: "pref_unavailable",
+                table: "profile",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.Sql(@"
+UPDATE profile
+  SET pref_unavailable = preference.pref_unavailable
+  FROM preference
+  WHERE preference.preference_id = profile.preference_id;
+");
+
+            migrationBuilder.DropColumn(
+                name: "pref_unavailable",
+                table: "preference");
 
             migrationBuilder.DropForeignKey(
                 name: "FK_job_preference_preference_id",
                 table: "job");
 
-            migrationBuilder.DropColumn(
-                name: "pref_unavailable",
-                table: "preference");
+            migrationBuilder.Sql(@"
+CREATE TEMP TABLE profile_job_temp
+AS SELECT profile_id, job_name, min(2, max(priority)) priority
+  FROM job
+    JOIN preference ON job.preference_id = preference.preference_id
+    JOIN profile ON profile.preference_id = preference.preference_id;
+");
+
+            migrationBuilder.Sql("DROP FROM job;");
 
             migrationBuilder.RenameColumn(
                 name: "preference_id",
@@ -153,6 +180,35 @@ SELECT preference_id, antag_name, priority FROM pref_antag_temp;
                 table: "job",
                 newName: "IX_job_profile_id");
 
+            migrationBuilder.AddForeignKey(
+                name: "FK_job_profile_profile_id",
+                table: "job",
+                column: "profile_id",
+                principalTable: "profile",
+                principalColumn: "profile_id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.Sql(@"
+INSERT INTO job(profile_id, job_name, priority)
+SELECT profile_id, job_name, priority FROM profile_job_temp;
+");
+
+            migrationBuilder.Sql("DROP TABLE profile_job_temp;");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_antag_preference_preference_id",
+                table: "antag");
+
+            migrationBuilder.Sql(@"
+CREATE TEMP TABLE profile_antag_temp
+AS SELECT profile_id, antag_name
+  FROM antag
+    JOIN preference ON antag.preference_id = preference.preference_id
+    JOIN profile ON profile.preference_id = preference.preference_id;
+");
+
+            migrationBuilder.Sql("DROP FROM antag;");
+
             migrationBuilder.RenameColumn(
                 name: "preference_id",
                 table: "antag",
@@ -163,13 +219,6 @@ SELECT preference_id, antag_name, priority FROM pref_antag_temp;
                 table: "antag",
                 newName: "IX_antag_profile_id_antag_name");
 
-            migrationBuilder.AddColumn<int>(
-                name: "pref_unavailable",
-                table: "profile",
-                type: "INTEGER",
-                nullable: false,
-                defaultValue: 0);
-
             migrationBuilder.AddForeignKey(
                 name: "FK_antag_profile_profile_id",
                 table: "antag",
@@ -178,13 +227,12 @@ SELECT preference_id, antag_name, priority FROM pref_antag_temp;
                 principalColumn: "profile_id",
                 onDelete: ReferentialAction.Cascade);
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_job_profile_profile_id",
-                table: "job",
-                column: "profile_id",
-                principalTable: "profile",
-                principalColumn: "profile_id",
-                onDelete: ReferentialAction.Cascade);
+            migrationBuilder.Sql(@"
+INSERT INTO antag(profile_id, antag_name)
+SELECT profile_id, antag_name, priority FROM profile_antag_temp;
+");
+
+            migrationBuilder.Sql("DROP TABLE profile_antag_temp;");
         }
     }
 }
